e47a9309807619d55abbccaf5f45b9717a255ee8
Naman Patel <naman.patel1@avantika.edu.in>
2026-02-11 20:50:08 +0530

    test code

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const images = [
+        "Background_image1.JPG",
+        "Background_image2.JPG",
+        "Background_image3.JPG",
+        "Background_image4.JPG"
+    ];
+
+    const slide1 = document.querySelector(".slide1");
+    const slide2 = document.querySelector(".slide2");
+
+    let currentIndex = 0;
+    let isSlide2Visible = false;
+
+    /* PRELOAD IMAGES */
+    images.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+
+    /* SET FIRST IMAGE */
+    slide1.style.backgroundImage = `url(${images[currentIndex]})`;
+
+    function changeSlide() {
+
+        currentIndex = (currentIndex + 1) % images.length;
+
+        if (!isSlide2Visible) {
+
+            slide2.style.backgroundImage = `url(${images[currentIndex]})`;
+            slide2.style.opacity = "1";
+
+        } else {
+
+            slide1.style.backgroundImage = `url(${images[currentIndex]})`;
+            slide2.style.opacity = "0";
+
+        }
+
+        isSlide2Visible = !isSlide2Visible;
+    }
+
+    setInterval(changeSlide, 5000);
+
+});
